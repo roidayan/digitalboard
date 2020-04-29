df09366330d5c8324fc2c20a5935469e6bedf35d
Roi Dayan <roi.dayan@gmail.com>
2020-04-29 08:54:48 -0500

    mb-msgs.php: Fix indent
    Signed-off-by: Roi Dayan <roi.dayan@gmail.com>

--- mb-msgs.php
+++ mb-msgs.php
@@ -7,64 +7,69 @@
  */
 
 class WPSE_85107 {
-  var $FOR_POST_TYPE = DBOARD_SCREEN_POST_TYPE;
-  var $SELECT_POST_TYPE = DBOARD_MSG_POST_TYPE;
-  var $box_id;
-  var $box_label;
-  var $field_id;
-  var $field_label;
-  var $field_name;
-  var $meta_key;
+	var $FOR_POST_TYPE = DBOARD_SCREEN_POST_TYPE;
+	var $SELECT_POST_TYPE = DBOARD_MSG_POST_TYPE;
+	var $box_id;
+	var $box_label;
+	var $field_id;
+	var $field_label;
+	var $field_name;
+	var $meta_key;
 
-  static function get_instance() {
-    return new WPSE_85107();
-  }
-  function __construct() {
-    add_action( 'admin_init', array( $this, 'admin_init' ) );
-  }
-  function admin_init() {
-    add_action( 'add_meta_boxes', array( $this, 'add_meta_boxes' ) );
-    add_action( 'save_post', array( $this, 'save_post' ), 10, 2 );
-    $this->meta_key     = "_selected_{$this->SELECT_POST_TYPE}";
-    $this->box_id       = "select-{$this->SELECT_POST_TYPE}-metabox";
-    $this->field_id     = "selected-{$this->SELECT_POST_TYPE}";
-    $this->field_name   = "selected_{$this->SELECT_POST_TYPE}";
-    $this->box_label    = __( "Select Messages" );
-    $this->field_label  = __( "No Messages" );
-  }
-  function add_meta_boxes() {
-    add_meta_box(
-      $this->box_id,
-      $this->box_label,
-      array( $this, 'select_box' ),
-      $this->FOR_POST_TYPE,
-      'side'
-    );
-  }
-  function select_box( $post ) {
-	  global $wp_post_types;
-	  $selected_post_id = get_post_meta( $post->ID, $this->meta_key, true );
-	  $save_hierarchical = $wp_post_types[$this->SELECT_POST_TYPE]->hierarchical;
-	  $wp_post_types[$this->SELECT_POST_TYPE]->hierarchical = true;
-	  wp1_checklist_pages( array(
-		  'id' => $this->field_id,
-		  'name' => $this->field_name,
-		  'checked' => empty( $selected_post_id ) ? 0 : $selected_post_id,
-		  'post_type' => $this->SELECT_POST_TYPE,
-		  'show_option_none' => $this->field_label,
-		  'sort_column' => 'post_date',
-	  ));
-	  $wp_post_types[$this->SELECT_POST_TYPE]->hierarchical = $save_hierarchical;
-  }
-  function save_post( $post_id, $post ) {
-	  if ( $post->post_type == $this->FOR_POST_TYPE ) {
-		  if ( isset( $_POST[$this->field_name] ) ) {
-			  update_post_meta( $post_id, $this->meta_key, $_POST[$this->field_name] );
-		  } else {
-			  delete_post_meta( $post_id, $this->meta_key );
-		  }
-	  }
-  }
+	static function get_instance() {
+		return new WPSE_85107();
+	}
+
+	function __construct() {
+		add_action( 'admin_init', array( $this, 'admin_init' ) );
+	}
+
+	function admin_init() {
+		add_action( 'add_meta_boxes', array( $this, 'add_meta_boxes' ) );
+		add_action( 'save_post', array( $this, 'save_post' ), 10, 2 );
+		$this->meta_key     = "_selected_{$this->SELECT_POST_TYPE}";
+		$this->box_id       = "select-{$this->SELECT_POST_TYPE}-metabox";
+		$this->field_id     = "selected-{$this->SELECT_POST_TYPE}";
+		$this->field_name   = "selected_{$this->SELECT_POST_TYPE}";
+		$this->box_label    = __( "Select Messages" );
+		$this->field_label  = __( "No Messages" );
+	}
+
+	function add_meta_boxes() {
+		add_meta_box(
+			$this->box_id,
+			$this->box_label,
+			array( $this, 'select_box' ),
+			$this->FOR_POST_TYPE,
+			'side'
+		);
+	}
+
+	function select_box( $post ) {
+		global $wp_post_types;
+		$selected_post_id = get_post_meta( $post->ID, $this->meta_key, true );
+		$save_hierarchical = $wp_post_types[$this->SELECT_POST_TYPE]->hierarchical;
+		$wp_post_types[$this->SELECT_POST_TYPE]->hierarchical = true;
+		wp1_checklist_pages( array(
+			'id' => $this->field_id,
+			'name' => $this->field_name,
+			'checked' => empty( $selected_post_id ) ? 0 : $selected_post_id,
+			'post_type' => $this->SELECT_POST_TYPE,
+			'show_option_none' => $this->field_label,
+			'sort_column' => 'post_date',
+		));
+		$wp_post_types[$this->SELECT_POST_TYPE]->hierarchical = $save_hierarchical;
+	}
+
+	function save_post( $post_id, $post ) {
+		if ( $post->post_type == $this->FOR_POST_TYPE ) {
+			if ( isset( $_POST[$this->field_name] ) ) {
+				update_post_meta( $post_id, $this->meta_key, $_POST[$this->field_name] );
+			} else {
+				delete_post_meta( $post_id, $this->meta_key );
+			}
+		}
+	}
 }
 
 add_action( 'init', array( 'WPSE_85107', 'get_instance' ) );
